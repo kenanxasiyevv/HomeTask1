f6b5c3e59e4533778133e9bc0a3024ac5f044fa2
kenanxasiyevv <kananakh@code.edu.az>
2022-12-28 02:43:21 +0400

    Add project files.

---
+++ HomeTask1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace HomeTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ededi daxil edin: ");
+            int number = int.Parse(Console.ReadLine());
+            int r = 0;
+            int sum = 0;
+
+            while (number >= 10)
+            {
+                r = number % 10;
+                number /= 100;
+
+                sum += r * r;
+
+
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
